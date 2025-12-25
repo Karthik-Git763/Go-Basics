8258d748e55261d496fb4e5bc5d0c92adf486af6
Karthik Das P <karthikpilakat@gmail.com>
2025-12-25 20:44:38 +0530

    Signup model checking errors and inserting in db

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -180,8 +180,29 @@ func (app *application) signUpUser(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
-	// Otherwise send a placeholder response (for now!)
-	fmt.Fprintf(w, "Create a new user...")
+	
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address is already in use")
+			app.render(w, r, "signup.page.tmpl", &templateData {
+				Form: form,
+			})
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	app.session.Put(r, "Flash", "Your Signup was successful. Please log in")
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+	
+	
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
